70954a4c2570d8ebbb123cac3fb442e2afe309b4
ABDul Rehman <devabdee@gmail.com>
2022-10-17 18:20:56 +0500

    .history removed

--- contracts/PodShipAuction.sol
+++ contracts/PodShipAuction.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.8;
-
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/utils/Address.sol";
-
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /**
