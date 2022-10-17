6ea4bacb7a714757bcb48e1efdc35e3e74c20f06
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-10-17 11:28:09 +0100

    uploaded basic auction template

---
+++ .history/contracts/PodNFT_20221017112535.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract PodNFt is ERC721 {
+
+  uint256 tokenId;
+
+  constructor() ERC721("PodShipNFT", "PodNFT") public {}
+    // podcast type: A - Audio & AV - Audio Video
+
+  function mint(address recipient, uint8 _podcastType, string memory podcastURI) external returns (uint256) {
+    tokenId = tokenId + 1;
+    _mint(recipient, tokenId);
+
+    return tokenId;
+  }
+  
+}
