a27918e310fad357a1469d0be1d86dbaa8cd2d41
ABDul Rehman <devabdee@gmail.com>
2022-10-22 12:50:20 +0500

    Core Functionalities Done

--- contracts/PodShipAuction.sol
+++ contracts/PodShipAuction.sol
@@ -19,7 +19,7 @@ contract PodShipAuction is PodShip {
         uint256 podcastId;
         uint256 reservePrice;
         uint256 startTime;
-        uint256 endTime;
+        uint256 duration;
         bool listed;
     }
 
@@ -33,18 +33,24 @@ contract PodShipAuction is PodShip {
     mapping(address => uint) public bids;
     
 
-    function startAuction(uint256 _podcastId, uint256 _reservePrice, uint256 _startTime, uint256 _endTime) public returns(uint256) {
+    function startAuction(uint256 _podcastId, uint256 _reservePrice, uint256 _duration) public returns(uint256) {
         require(msg.sender == ownerOf(_podcastId), "only NFT Owner can start the Auction");
+        require(_duration >= 1 && _duration <= 7, "Auction duration can be between 1-7 Days");
         auctionId.increment();
         approve(address(this), podcastId[_podcastId].tokenId);
-        auctions[auctionId.current()] = Auction(_podcastId, _reservePrice, _startTime, _endTime, true);
+        // uint256 auction_duration = _duration * 86400; ///// For Mainnet
+        uint256 auction_duration = _duration * 60; ///// For testnet-testing
+        auctions[auctionId.current()] = Auction(_podcastId, _reservePrice, 0, auction_duration, true);
         return auctionId.current();
     }
 
     function bid(uint256 _auctionId) public payable {
         require(auctions[_auctionId].listed, "NFT not on Auction");
-        require(block.timestamp > auctions[_auctionId].startTime, "Auction not started yet");
-        require(block.timestamp < auctions[_auctionId].endTime, "Auction Ended");
+        if(bidders[_auctionId].highestBidder == address(0)) {
+            auctions[_auctionId].startTime = block.timestamp;
+        }
+        auctions[_auctionId].duration = auctions[_auctionId].startTime + auctions[_auctionId].duration;
+        require(block.timestamp < auctions[_auctionId].duration, "Auction Ended");
         require(msg.value > auctions[_auctionId].reservePrice && msg.value > bidders[_auctionId].highestBid, "Input amount below NFT's reservePrice or last Bid");
         if (msg.sender != address(0)) {
             bids[msg.sender] += msg.value;
@@ -53,13 +59,15 @@ contract PodShipAuction is PodShip {
         bidders[_auctionId].highestBid = msg.value;
     }
 
+    // Reentrancy Gaurd needed here:
     function endAuction(uint256 _auctionId) public payable {
         require(auctions[_auctionId].listed, "NFT not on Auction");
-        require(block.timestamp > auctions[_auctionId].endTime, "Auction In Progress");
+        require(block.timestamp > auctions[_auctionId].startTime + auctions[_auctionId].duration, "Auction In Progress");
         require(msg.sender == podcastId[auctions[_auctionId].podcastId].nftOwner || msg.sender == bidders[_auctionId].highestBidder, "Only Auction Creator & Winner allowed");
         auctions[_auctionId].listed = false;
         safeTransferFrom(podcastId[auctions[_auctionId].podcastId].nftOwner, bidders[_auctionId].highestBidder, podcastId[auctions[_auctionId].podcastId].tokenId);
         (bool success, ) = (podcastId[auctions[_auctionId].podcastId].nftOwner).call{value: bidders[_auctionId].highestBid}("");
+        podcastId[auctions[_auctionId].podcastId].nftOwner = bidders[_auctionId].highestBidder;
         require(success, "NFT Tranfership Failed");
     }
 
